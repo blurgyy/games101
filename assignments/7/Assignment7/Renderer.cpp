95e59869cde3419eb32f230864ea7467155bc9f2
Blurgyy <blurgyy@gmail.com>
2020-04-18 21:14:07 +0800

    Optimized multi-threading a little
    Signed-off-by: Blurgyy <blurgyy@gmail.com>

--- assignments/7/Assignment7/Renderer.cpp
+++ assignments/7/Assignment7/Renderer.cpp
@@ -22,12 +22,12 @@ void Renderer::Render(const Scene& scene)
     float scale = tan(deg2rad(scene.fov * 0.5));
     float imageAspectRatio = scene.width / (float)scene.height;
     Vector3f eye_pos(278, 273, -800);
-    int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 32;
+    int spp = 64;
     std::cout << "SPP: " << spp << "\n";
     for (uint32_t j = 0; j < scene.height; ++j) {
+        #pragma omp parallel for
         for (uint32_t i = 0; i < scene.width; ++i) {
             // generate primary ray direction
             float x = (2 * (i + 0.5) / (float)scene.width - 1) *
@@ -35,12 +35,11 @@ void Renderer::Render(const Scene& scene)
             float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
 
             Vector3f dir = normalize(Vector3f(-x, y, 1));
-            #pragma omp parallel for
+            int ind = j * scene.width + i;
             for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0);
+                framebuffer[ind] += scene.castRay(Ray(eye_pos, dir), 0);
             }
-            framebuffer[m] = framebuffer[m] / spp;
-            m++;
+            framebuffer[ind] = framebuffer[ind] / spp;
         }
         UpdateProgress(j / (float)scene.height);
     }
