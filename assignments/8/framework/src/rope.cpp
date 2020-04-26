adab527fc5558cfbfd8729eb5fce9ae34795aecb
Blurgyy <blurgyy@gmail.com>
2020-04-26 16:17:34 +0800

    Explicit/Semi-implicit Euler method
    todo:
    - verlet integration
    - global damping
    
    Signed-off-by: Blurgyy <blurgyy@gmail.com>

--- assignments/8/framework/src/rope.cpp
+++ assignments/8/framework/src/rope.cpp
@@ -12,11 +12,20 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
-
+        for(int i = 0; i < num_nodes; ++ i){
+            Vector2D pos = start + i * (end - start) / (num_nodes - 1);
+            Mass* mass = new Mass(pos, node_mass, false);
+            mass->velocity = Vector2D(0, 0);
+            masses.push_back(mass);
+        }
+        for(int i = 1; i < num_nodes; ++ i){
+            Spring* spring = new Spring(masses[i-1], masses[i], k);
+            springs.push_back(spring);
+        }
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+       for (auto &i : pinned_nodes) {
+           masses[i]->pinned = true;
+       }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
@@ -24,6 +33,13 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            //  x -------- x
+            // m1          m2
+            Vector2D m1tom2 = s->m2->position - s->m1->position;
+            double curlen = m1tom2.norm();
+            Vector2D force = s->k * m1tom2 / curlen * (curlen - s->rest_length);
+            s->m1->forces += force;
+            s->m2->forces -= force;
         }
 
         for (auto &m : masses)
@@ -31,7 +47,15 @@ namespace CGL {
             if (!m->pinned)
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                m->forces += gravity;
+                Vector2D accel = m->forces / m->mass;
+                // Explicit Euler method
+                // m->position += m->velocity * delta_t;
+                // m->velocity += accel * delta_t;
 
+                // Semi-implicit Euler method
+                m->velocity += accel * delta_t;
+                m->position += m->velocity * delta_t;
                 // TODO (Part 2): Add global damping
             }
 
